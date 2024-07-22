a8246497a9dead6711cf3369a97ac9d8542b1098
byb0823 <byb0823@naver.com>
2024-07-22 21:30:00 +0900

    feat: Update return value
    - 리턴 값 수정

--- src/main/java/hello/companysystem/api/ApiControllerAdvice.java
+++ src/main/java/hello/companysystem/api/ApiControllerAdvice.java
@@ -1,13 +1,12 @@
 package hello.companysystem.api;
 
-import lombok.Builder;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.net.BindException;
 
 @Getter
 @RestControllerAdvice
@@ -16,7 +15,10 @@ public class ApiControllerAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(BindException.class)
     public ApiResponse<Object> bindException(BindException e) {
-        return null;
+        return ApiResponse.of(
+                HttpStatus.BAD_REQUEST,
+                e.getBindingResult().getAllErrors().get(0).getDefaultMessage(),
+                null);
     }
 
 }
