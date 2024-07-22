69c20da4fe03d7053fbde02d883d255e9b2ec7ae
byb0823 <byb0823@naver.com>
2024-07-22 17:18:54 +0900

    test: Add test of() method
    - of의 테스트 코드를 추가하여 TeamResponse 반환을 테스트 함

---
+++ src/test/java/hello/companysystem/api/service/team/response/TeamResponseTest.java
@@ -0,0 +1,33 @@
+package hello.companysystem.api.service.team.response;
+
+import hello.companysystem.domain.team.Team;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TeamResponseTest {
+
+    @DisplayName("팀을 저장하면 팀과 관련된 정보를 반환한다.")
+    @Test
+    void of() {
+        //given
+        Team team = Team.builder()
+                .teamNumber("001")
+                .teamName("teamA")
+                .build();
+
+        //when
+        TeamResponse teamResponse = TeamResponse.of(team);
+
+        //then
+        assertThat(teamResponse)
+                .extracting("teamNumber", "teamName")
+                .contains("001", "teamA");
+    }
+
+}
