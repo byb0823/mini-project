a270f02658d623ec90daef4c7e4692ce30846453
byb0823 <byb0823@naver.com>
2024-07-22 18:57:35 +0900

    feat: Add toServiceRequest test
    - TeamCreateServiceRequest로 변환하는 기능 추가

---
+++ src/main/java/hello/companysystem/api/controller/team/request/TeamCreateRequest.java
@@ -0,0 +1,24 @@
+package hello.companysystem.api.controller.team.request;
+
+import hello.companysystem.api.service.team.request.TeamCreateServiceRequest;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class TeamCreateRequest {
+
+    @NotBlank(message = "팀 이름을 입력해주세요.")
+    private String teamName;
+
+    @Builder
+    private TeamCreateRequest(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public TeamCreateServiceRequest toServiceRequest() {
+        return TeamCreateServiceRequest.builder()
+                .teamName(this.teamName)
+                .build();
+    }
+}
