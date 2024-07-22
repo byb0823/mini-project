d6f484b69ff50343eb1dab08ed072d36d7a80b0e
byb0823 <byb0823@naver.com>
2024-07-22 18:14:39 +0900

    refactor: Update return value and parameter
    - 반환값과 파라미터를 설정함

---
+++ src/main/java/hello/companysystem/api/controller/team/TeamController.java
@@ -0,0 +1,24 @@
+package hello.companysystem.api.controller.team;
+
+import hello.companysystem.api.ApiResponse;
+import hello.companysystem.api.controller.team.request.TeamCreateRequest;
+import hello.companysystem.api.service.team.TeamService;
+import hello.companysystem.api.service.team.response.TeamResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class TeamController {
+
+    private final TeamService teamService;
+
+    @PostMapping("/teams")
+    public ApiResponse<TeamResponse> creatTeam(@Valid @RequestBody TeamCreateRequest request) {
+        return null;
+    }
+
+}
