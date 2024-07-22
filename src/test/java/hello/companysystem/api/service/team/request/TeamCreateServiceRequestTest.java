48f68b3cca96b25047f520e2f1775c7d0cf7e8eb
byb0823 <byb0823@naver.com>
2024-07-22 21:28:25 +0900

    refactor: Rename TeamSaveServiceRequest To TeamCreateServiceRequest
    - 클래스 이름 변경

--- src/test/java/hello/companysystem/api/service/team/request/TeamCreateServiceRequestTest.java
+++ src/test/java/hello/companysystem/api/service/team/request/TeamCreateServiceRequestTest.java
@@ -1,21 +1,19 @@
 package hello.companysystem.api.service.team.request;
 
 import hello.companysystem.domain.team.Team;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
-class TeamSaveServiceRequestTest {
+class TeamCreateServiceRequestTest {
 
     @DisplayName("팀 번호를 받아 팀 엔티티를 생성한다.")
     @Test
     void toEntity() {
         //given
         String nextTeamNumber = "001";
-        TeamSaveServiceRequest request = TeamSaveServiceRequest.builder()
+        TeamCreateServiceRequest request = TeamCreateServiceRequest.builder()
                 .teamName("teamA")
                 .build();
 
