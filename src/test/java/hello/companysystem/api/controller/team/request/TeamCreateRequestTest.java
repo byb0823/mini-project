9c73e501bea5480577dd43e8ab7220a3c02705b6
byb0823 <byb0823@naver.com>
2024-07-22 18:57:21 +0900

    test: Add toServiceRequest test
    - TeamCreateServiceRequest로 변환하는 기능 추가

---
+++ src/test/java/hello/companysystem/api/controller/team/request/TeamCreateRequestTest.java
@@ -0,0 +1,26 @@
+package hello.companysystem.api.controller.team.request;
+
+import hello.companysystem.api.service.team.request.TeamCreateServiceRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class TeamCreateRequestTest {
+
+    @DisplayName("컨트롤러에서 사용하는 dto는 서비스에서 사용하는 객체로 바꿔줘야 한다.")
+    @Test
+    void toServiceRequest() {
+        //given
+        TeamCreateRequest request = TeamCreateRequest.builder()
+                .teamName("teamA")
+                .build();
+
+        //when
+        TeamCreateServiceRequest serviceRequest = request.toServiceRequest();
+
+        //then
+        assertThat(serviceRequest.getTeamName()).isEqualTo("teamA");
+    }
+
+}
