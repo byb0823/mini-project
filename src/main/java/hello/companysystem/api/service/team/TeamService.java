49237d3545a81dbf6a06344ca199fb0de308b862
byb0823 <byb0823@naver.com>
2024-07-22 17:25:43 +0900

    feat: Add createTeam method - Team을 생성하여 저장하고 결과를 반환함 - TeamSaveServiceRequest를 받아서 Team을 생성함 - Team을 생성할 때 팀 번호와 이름을 입력함 - Team을 저장하고 TeamResponse로 변환하여 반환함

--- src/main/java/hello/companysystem/api/service/team/TeamService.java
+++ src/main/java/hello/companysystem/api/service/team/TeamService.java
@@ -1,8 +1,34 @@
 package hello.companysystem.api.service.team;
 
+import hello.companysystem.api.service.team.request.TeamSaveServiceRequest;
+import hello.companysystem.api.service.team.response.TeamResponse;
+import hello.companysystem.domain.team.Team;
+import hello.companysystem.domain.team.TeamRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 @Service
 public class TeamService {
 
+    private final TeamRepository teamRepository;
+
+    @Transactional
+    public TeamResponse createTeam(TeamSaveServiceRequest request) {
+        String nextTeamNumber = getNextTeamNumber();
+        Team team = request.toEntity(nextTeamNumber);
+        Team savedTeam = teamRepository.save(team);
+        return TeamResponse.of(savedTeam);
+    }
+
+    private String getNextTeamNumber() {
+        String latestTeamNumber = teamRepository.findLatestTeamNumber();
+        if (latestTeamNumber == null) {
+            return "001";
+        }
+        int nextTeamNumber = Integer.parseInt(latestTeamNumber) + 1;
+        return String.format("%03d", nextTeamNumber);
+    }
 }
