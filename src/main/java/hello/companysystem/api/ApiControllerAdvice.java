e1d5beab52f7a4e7c1a683f527d280f895cb9dfa
byb0823 <byb0823@naver.com>
2024-07-22 17:57:25 +0900

    feat: Add ApiControllerAdvice
    - BindException 공통 예외처리를 추가함

---
+++ src/main/java/hello/companysystem/api/ApiControllerAdvice.java
@@ -0,0 +1,22 @@
+package hello.companysystem.api;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.net.BindException;
+
+@Getter
+@RestControllerAdvice
+public class ApiControllerAdvice {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BindException.class)
+    public ApiResponse<Object> bindException(BindException e) {
+        return null;
+    }
+
+}
