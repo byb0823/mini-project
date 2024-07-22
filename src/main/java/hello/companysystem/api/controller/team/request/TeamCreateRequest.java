babb18cdfef8e90c936a245b08efc88fe0c8d31b
byb0823 <byb0823@naver.com>
2024-07-22 21:25:22 +0900

    feat: Add NoArgsConstructor
    - 파라미터 없는 생성자 추가

--- src/main/java/hello/companysystem/api/controller/team/request/TeamCreateRequest.java
+++ src/main/java/hello/companysystem/api/controller/team/request/TeamCreateRequest.java
@@ -4,8 +4,10 @@ import hello.companysystem.api.service.team.request.TeamCreateServiceRequest;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class TeamCreateRequest {
 
     @NotBlank(message = "팀 이름을 입력해주세요.")
