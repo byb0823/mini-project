0ed94688da769d68e09ae97338339094dfa885a0
byb0823 <byb0823@naver.com>
2024-07-25 11:34:19 +0900

    test: Add EmployeeRepository test
    - EmployeeRepository 테스트 작성

---
+++ src/test/java/hello/companysystem/domain/employee/EmployeeRepositoryTest.java
@@ -0,0 +1,62 @@
+package hello.companysystem.domain.employee;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.LocalDate;
+
+import static hello.companysystem.domain.employee.Role.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ActiveProfiles("test")
+@DataJpaTest
+class EmployeeRepositoryTest {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @AfterEach
+    void tearDown() {
+        employeeRepository.deleteAllInBatch();
+    }
+
+    @DisplayName("등록된 직원이 없으면 null을 반환한다.")
+    @Test
+    void findLatestTeamNumberWithoutEmployee() {
+        //given //when
+        String latestEmployeeNumber = employeeRepository.findLatestEmployeeNumber();
+
+        //then
+        assertThat(latestEmployeeNumber).isNull();
+    }
+
+    @DisplayName("가장 마지막에 등록된 직원의 직원 번호를 반환한다.")
+    @Test
+    void findLatestTeamNumber() {
+        //given
+        LocalDate joinDate = LocalDate.of(2020, 1, 1);
+        LocalDate birthday = LocalDate.of(2001, 1, 1);
+        Employee employee = Employee.builder()
+                .employeeNumber("001")
+                .name("nameA")
+                .role(MEMBER)
+                .joinDate(joinDate)
+                .birthday(birthday)
+                .team(null)
+                .build();
+        employeeRepository.save(employee);
+
+        //when
+        String latestEmployeeNumber = employeeRepository.findLatestEmployeeNumber();
+
+        //then
+        assertThat(latestEmployeeNumber).isEqualTo("001");
+    }
+
+}
